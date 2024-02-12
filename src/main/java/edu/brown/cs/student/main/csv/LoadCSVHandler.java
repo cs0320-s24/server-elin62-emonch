136d2c0db39dbf365ff3f073a1a770586f0264c6
emilylin <emily_lin1@brown.edu>
2024-02-12 13:04:18 -0500

    Created structure

---
+++ src/main/java/edu/brown/cs/student/main/csv/LoadCSVHandler.java
@@ -0,0 +1,12 @@
+package edu.brown.cs.student.main.csv;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class LoadCSVHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
